6c15dd1bdea8e8564547ab021dd02b1c8e1e6b37
Latisha Faroul <Lfaroul14@gmail.com>
2021-05-17 22:55:44 -0400

    commit tree 987cd63ff3ae3cd65b90d22ca69cf83103592b0b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Louie Loki';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'; 
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = 'Welcome, ' + currentUser[0] + '!';
